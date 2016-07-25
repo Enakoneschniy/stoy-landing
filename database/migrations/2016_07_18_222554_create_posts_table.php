05acfef619eb1746555437357e323cf2aa07d01b
Evgeniy <e.nakoneschniy@gmai.com>
2016-07-25 21:22:57 +0300

    Gallery, Post

---
+++ database/migrations/2016_07_18_222554_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('preview_text');
+            $table->longText('detail_text');
+            $table->string('type');
+            $table->string('preview_img');
+            $table->string('detail_img');
+            $table->string('video');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}

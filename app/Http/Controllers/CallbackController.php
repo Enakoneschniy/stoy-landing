f2656641be1c2482886d5ff23414d8f64b83af9c
romakov <r.kovalenko@hotmail.com>
2016-08-15 00:49:34 +0300

    Update some function

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Callback;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,7 +17,16 @@ class CallbackController extends Controller
         {
             $callback->name = $request->name;
             $callback->phone = $request->phone;
+            $callback->active = 1;
+            $callback->status = 'Активный';
             $callback->save();
+            $arrCallback = $callback->toArray();
+            Mail::send('emails.callback', $arrCallback, function($message) use ($arrCallback)
+            {
+                $message->from(env('MAIL_USERNAME'), 'Profteplostroy')
+                    ->subject('Добавление новой заявки')
+                    ->to(env('MAIL_USERNAME'));
+            });
             return 'Add callback';
         } else {
             return 'Error!';
--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.yandex.ru'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,8 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'evgenij.nakoneschniy@yandex.ru', 'name' => 'Evgeniy'],
+
 
     /*
     |--------------------------------------------------------------------------
@@ -68,7 +69,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
 
     /*
     |--------------------------------------------------------------------------
